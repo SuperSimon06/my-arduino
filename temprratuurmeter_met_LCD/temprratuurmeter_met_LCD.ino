ffa8174927af4fe5e49ba4a527b07990e3b15852
SuperSimon06 <simongvanwalsum@gmail.com>
2021-08-09 14:34:01 +0200

    Testen op nan toegevoegd.

--- temprratuurmeter_met_LCD/temprratuurmeter_met_LCD.ino
+++ temprratuurmeter_met_LCD/temprratuurmeter_met_LCD.ino
@@ -4,10 +4,10 @@
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 9, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-
 #define DHTTYPE    DHT11     // DHT 11
-#define DHT11_PIN 7
+#define DHT11_PIN 13
 DHT dht(DHT11_PIN, DHTTYPE);
+
 
 
 void setup(){
@@ -23,11 +23,20 @@ void loop()
   float tempc = dht.readTemperature();
   
   lcd.setCursor(0,0); 
-  lcd.print("Temp.in°c: ");
-  lcd.print(tempc);
+  if (isnan(tempc)) {
+      lcd.print("Temp. niet te lezen");
+  } else {
+      lcd.print("Temp.in'c: ");
+      lcd.print(tempc);
+  }
   lcd.setCursor(0,1);
-  lcd.print("humidity: ");
-  lcd.print(vochtinlucht);
-  lcd.print("%");
+  if (isnan(vochtinlucht)) {
+      lcd.print("Vocht niet te lezen");
+  } else {
+      lcd.print("humidity: ");
+      lcd.print(vochtinlucht);
+      lcd.print("%");
+    
+  }
 }
 
